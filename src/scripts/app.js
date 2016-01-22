a734b7bed68c3a42b570310d6cc6fd53813ba718
Yuriy Boychenko <yuboychenko@gmail.com>
2016-01-22 19:14:16 +0200

    ngRoute was removed @TODO Add ability to inargate with ui-router(states)

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -3,44 +3,15 @@
 var defaultSettings = {
   'appID'          : '',
   'permissions'    : '',
-  'channelFile'    : 'bower_components/angular-facebook-utils/channel.html',
-  'routingEnabled' : false,
-  'loginPath'      : '/'
+  'channelFile'    : 'bower_components/angular-facebook-utils/channel.html'
 };
 
-var application = angular.module('facebookUtils', ['ngRoute'])
+var application = angular.module('facebookUtils', [])
   .constant('facebookConfigDefaults', defaultSettings)
   .constant('facebookConfigSettings', defaultSettings)
   .run([
-    'facebookConfigSettings', 'facebookConfigDefaults', '$rootScope', '$location', 'facebookUser', '$route',
-    function(facebookConfigSettings, facebookConfigDefaults, $rootScope, $location, facebookUser, $route) {
-
-      //handle routing
-      if (facebookConfigSettings.routingEnabled) {
-        $rootScope.$on('$routeChangeStart', function(event, next, current) {
-
-          if (next && next.$$route && next.$$route.needAuth) {
-            facebookUser.then(function(user) {
-              if (!user.loggedIn) {
-                // reload the login route
-                $location.path(facebookConfigSettings.loginPath || facebookConfigDefaults.loginPath);
-              }
-            });
-          }
-          /*
-          * NOTE:
-          * It's important to repeat the control also in the backend,
-          * before sending back from the server reserved information.
-          */
-        });
-
-        $rootScope.$on('fbLogoutSuccess', function() {
-
-          if ($route.current.$$route.needAuth) {
-            // reload the login route
-            $location.path(facebookConfigSettings.loginPath || facebookConfigDefaults.loginPath);
-          }
-        });
-      }
+    'facebookConfigSettings', 'facebookConfigDefaults', '$rootScope', '$location', 'facebookUser',
+    function(facebookConfigSettings, facebookConfigDefaults, $rootScope, $location, facebookUser) {
+      
     }
   ]);
